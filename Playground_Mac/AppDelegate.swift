be098b316d17742a8b120393e384d36c0cd8e756
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-11-22 15:33:53 -0800

    initial commit.

---
+++ Playground_Mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Playground_Mac
+//
+//  Created by Jeffrey Bergier on 11/22/15.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
